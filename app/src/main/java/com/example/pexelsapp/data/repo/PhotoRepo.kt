8f8fae9fdd02cbe2c309a79965b67c35d12011c5
Sean Rich <richsean56@gmail.com>
2020-08-25 11:41:25 -0400

    Added PhotoRepo.kt

---
+++ app/src/main/java/com/example/pexelsapp/data/repo/PhotoRepo.kt
@@ -0,0 +1,24 @@
+package com.example.pexelsapp.data.repo
+
+import com.example.pexelsapp.BuildConfig.API_KEY
+import com.example.pexelsapp.data.api.PhotoDataSource
+import com.example.pexelsapp.utils.performGetOperation
+import javax.inject.Inject
+
+class PhotoRepo @Inject constructor(
+    private val photoDataSource: PhotoDataSource,
+    private val localDataSource: PhotoDao
+) {
+
+    fun getPhoto(id: Int) = performGetOperation(
+        databaseQuery = { localDataSource.getPhoto(id) },
+        networkCall = { photoDataSource.getPhoto(id, API_KEY) },
+        saveCallResult = { localDataSource.insert(it) }
+    )
+
+    fun getPhotos(search: String, perPage: Int, page: Int) = performGetOperation (
+        databaseQuery = { localDataSource.getAllPhotos() },
+        networkCall = { photoDataSource.getPhotos(search, perPage, page, API_KEY)},
+        saveCallResult = { it.photos?.let { items -> localDataSource.insertAll(items) } }
+    )
+}
