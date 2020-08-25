cb652e5cfbda21776d6c2b231c9cb4bef1fc5dfd
Sean Rich <richsean56@gmail.com>
2020-08-25 12:40:35 -0400

    Added View Picasso, Updated PhotoAdapter.kt to work with Picasso width and height

---
+++ app/src/main/java/com/example/pexelsapp/utils/extensions/View.kt
@@ -0,0 +1,24 @@
+package com.example.pexelsapp.utils.extensions
+
+import android.view.View
+import android.widget.ImageView
+import com.example.pexelsapp.R
+import com.squareup.picasso.Picasso
+
+fun ImageView.loadUrl(url: String?, width: Int, height: Int) {
+    Picasso.with(context)
+        .load(url)
+        .resize(width, height)
+        .centerCrop()
+        .placeholder(R.drawable.ic_launcher_foreground)
+        .error(R.drawable.ic_launcher_foreground)
+        .into(this)
+}
+
+fun View.show(show: Boolean? = true) {
+    visibility = when (show) {
+        true -> View.VISIBLE
+        false -> View.GONE
+        else -> View.INVISIBLE
+    }
+}
