2e17257ab34450d84c2b13e618e25b6bd5c1d3ec
NicholasHeilman <Nicholas.Heilman@gmail.com>
2020-08-25 11:31:49 -0400

    add BaseDataSource, PhotoDataSource, and PhotoService

---
+++ app/src/main/java/com/example/pexelsapp/data/api/PhotoService.kt
@@ -0,0 +1,26 @@
+package com.example.pexelsapp.data.api
+
+import com.example.pexelsapp.data.model.Photo
+import com.example.pexelsapp.data.model.PhotoSearchResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface PhotoService {
+
+    @GET("search")
+    suspend fun getPhotos(
+        @Query("query") search: String,
+        @Query("per_page") per_page: Int,
+        @Query("page") page: Int,
+        @Header("Authorization") api_key: String
+    ): Response<PhotoSearchResponse>
+
+    @GET("photos/{id}")
+    suspend fun getPhoto(
+        @Path("id") id: Int,
+        @Header("Authorization") api_key: String
+    ): Response<Photo>
+}
