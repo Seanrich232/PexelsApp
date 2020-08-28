260aef512a415539165f870a40b315ebad6643b3
NicholasHeilman <Nicholas.Heilman@gmail.com>
2020-08-28 11:03:58 -0400

    DO NOT MERGE commiting to change computers

--- app/src/main/java/com/example/pexelsapp/data/api/PhotoService.kt
+++ app/src/main/java/com/example/pexelsapp/data/api/PhotoService.kt
@@ -1,14 +1,13 @@
 package com.example.pexelsapp.data.api
 
+import com.example.pexelsapp.BuildConfig.API_KEY
 import com.example.pexelsapp.data.model.Photo
 import com.example.pexelsapp.data.model.PhotoSearchResponse
 import retrofit2.Response
-import retrofit2.http.GET
-import retrofit2.http.Header
-import retrofit2.http.Path
-import retrofit2.http.Query
+import retrofit2.http.*
 
 interface PhotoService {
+//    @Headers(HEADER_AUTHORIZATION)
 
     @GET("search")
     suspend fun getPhotos(
@@ -23,4 +22,16 @@ interface PhotoService {
         @Path("id") id: Int,
         @Header("Authorization") api_key: String
     ): Response<Photo>
+
+    @GET("search")
+    suspend fun photoSearch(
+        @Query("query") search: String,
+        @Query("per_page") per_page: Int,
+        @Query("page") page: Int,
+        @Header("Authorization") api_key: String
+    ) : PhotoSearchResponse
+
+//    companion object{
+//        const val HEADER_AUTHORIZATION = "Authorization:$API_KEY"
+//    }
 }
--- app/src/main/java/com/example/pexelsapp/ui/photos/PhotoSearchFragment.kt
+++ app/src/main/java/com/example/pexelsapp/ui/photos/PhotoSearchFragment.kt
@@ -1,6 +1,8 @@
 package com.example.pexelsapp.ui.photos
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,12 +11,16 @@ import androidx.core.os.bundleOf
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.pexelsapp.utils.autoCleared
 import com.example.pexelsapp.R
 import com.example.pexelsapp.databinding.FragmentPhotoSearchBinding
 import dagger.hilt.android.AndroidEntryPoint
 import com.example.pexelsapp.utils.Resource
+import com.example.pexelsapp.utils.extensions.showView
 import com.example.pexelsapp.utils.extensions.toast
+import kotlinx.android.synthetic.main.toolbar_snippet.*
+import timber.log.Timber
 import java.util.*
 
 @AndroidEntryPoint
@@ -23,20 +29,51 @@ class PhotoSearchFragment : Fragment(), PhotoAdapter.PhotoItemListener {
     private var binding: FragmentPhotoSearchBinding by autoCleared()
     private val viewModel: PhotoViewModel by viewModels()
     private lateinit var adapter: PhotoAdapter
+    var query: CharSequence? = ""
+
+    private val textWatcher = object : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {
+            // NO  OPERATION
+        }
+
+        override fun beforeTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            // NO  OPERATION
+        }
+
+        override fun onTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            val isBackSpace = query?.length ?: -1 >= s?.length ?: -1
+            query = s.toString()
+            iv_Clear?.showView(true)
+
+            s?.let {
+                if (it.isNotEmpty() && !isBackSpace) {
+                    Timber.i(it.toString())
+                    viewModel.fetchPhoto("${it.toString()}")
+                }
+            }
+        }
+    } //end textWatcher
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentPhotoSearchBinding.inflate(inflater, container, false)
-        return binding.root
+    ): View? = FragmentPhotoSearchBinding.inflate(inflater, container, false).apply {
+        RecyclerView.LayoutManager = LinearLayoutManager(context)
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupObservers()
         setupRecyclerView()
+        activity?.et_toolbarSearch?.addTextChangedListener(textWatcher)
+
+//        iv_Clear.setOnClickListener{
+//
+//            et_toolbarSearch.text?.clear()
+//            iv_Clear.showView(false)
+//        }
     }
 
     private fun setupRecyclerView() {
